f70bf1228acc983ede5075e0c0a31561205c8203
valerarenny <valerarenny@gmail.com>
2024-06-22 13:30:54 -0400

    mi primer commit hackaton rimac

---
+++ src/main/java/com/caribes/caribesapi/CaribesapiApplication.java
@@ -0,0 +1,13 @@
+package com.caribes.caribesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaribesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaribesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caribes/caribesapi/CaribesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caribes.caribesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaribesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
